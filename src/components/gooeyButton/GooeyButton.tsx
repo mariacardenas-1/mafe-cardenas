1102c03786071534207b16a74c12d6591fa1f896
mafe cardenas <maria.cardenas.r3@gmail.com>
2023-07-27 18:15:07 -0300

    fix new ui

--- src/components/gooeyButton/GooeyButton.tsx
+++ src/components/gooeyButton/GooeyButton.tsx
@@ -7,7 +7,7 @@ export default function GooeyButton() {
             <div className={style.wrapper}>
                 <Link className={style.button} href="mailto:maria.cardenas.r3@gmail.com">Contrátame!</Link>
             </div>
-            <svg style={{visibility: 'hidden', position: 'absolute', width: "0", height: "0", xmlns: "http://www.w3.org/2000/svg"}} version="1.1">
+            <svg style={{visibility: 'hidden', position: 'absolute', width: "0", height: "0"}} version="1.1">
                 <defs>
                     <filter id="goo"><feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />    
                         <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="goo" />
